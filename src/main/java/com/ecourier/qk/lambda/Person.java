0436f9cd9b1195c84ef683bf437825014c2df53a
quarkusio <no-reply@quarkus.io>
2022-02-10 17:57:11 +0000

    Initial commit

---
+++ src/main/java/com/ecourier/qk/lambda/Person.java
@@ -0,0 +1,15 @@
+package com.ecourier.qk.lambda;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
